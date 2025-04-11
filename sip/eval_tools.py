9c1d5e3e6ed143bd68ceda64a4345cd45da2a614
Micha Elsner <osu8210@ascend-rw01.ten.osc.edu>
2025-04-11 11:39:28 -0400

    now evaluate informativeness

--- sip/eval_tools.py
+++ sip/eval_tools.py
@@ -2,7 +2,7 @@ import torch
 
 import Levenshtein
 
-from collections import deque
+from collections import deque, Counter
 
 from sip.data_gen.utils import write_tsv
 
@@ -29,18 +29,58 @@ class MovingAvg:
         self.l.append(x)
         return sum(self.l) / len(self.l)
 
+def add_binary_stats(stats, results):
+    for (inp, gold, pred) in results:
+        inp = inp.rstrip(u"\x13")
+        if inp == gold:
+            trial_type = "neg"
+        else:
+            trial_type = "pos"
+
+        if inp == pred:
+            resp_type = "neg"
+        elif gold == pred:
+            resp_type = "corr"
+        else:
+            resp_type = "err"
+
+        #print("Trial:", inp, gold, pred, trial_type, resp_type)
+        stats[(trial_type, resp_type)] += 1
+
+def binary_metrics(stats):
+    nn = sum(stats.values())
+
+    totalPos = 0
+    totalNeg = 0
+    for (ttp, rtp), ct in stats.items():
+        if ttp == "pos":
+            totalPos += ct
+        else:
+            totalNeg += ct
+
+    #print("total", nn, totalPos, totalNeg)
+    assert(totalPos + totalNeg == nn)
+
+    tpr = stats[("pos", "corr")] / totalPos
+    fpr = stats[("neg", "err")] / totalNeg
+    tnr = stats[("neg", "neg")] / totalNeg
+    ifn = tpr + tnr - 1
+    return { "tpr" : tpr, "fpr" : fpr, "tnr" : tnr, "inform" : ifn }
 
 def evaluate_on(model, tokenizer, dataloader, log_result=None):
   correct, total, edit_dist, per = 0,0,0,0
+  binary_stats = Counter()
   model.eval()
   all_preds = []
   for test_batch in dataloader:
     test_batch = {k: v.to(model.device) for k,v in test_batch.items()}
     test_batch_inputs = dict(test_batch)
+    inp = tokenizer.batch_decode(100*(test_batch_inputs["input_ids"] == -100) + test_batch_inputs["input_ids"], skip_special_tokens=True) # replace -100 by 0
     del test_batch_inputs["labels"]
     r = tokenizer.batch_decode(model.generate(**test_batch_inputs, max_new_tokens=test_batch["labels"].shape[1]+2,
                                               early_stopping="never", num_beams=1, no_repeat_ngram_size=0), skip_special_tokens=True)
     gold = tokenizer.batch_decode(100*(test_batch["labels"] == -100) + test_batch["labels"], skip_special_tokens=True) # replace -100 by 0
+    add_binary_stats(binary_stats, zip(inp, gold, r))
 
     if log_result != None:
         all_preds += list(zip(gold, r))
@@ -52,7 +92,7 @@ def evaluate_on(model, tokenizer, dataloader, log_result=None):
 
   if log_result != None:
       write_tsv(log_result, all_preds)
-  return correct/total, edit_dist/total, per/total
+  return correct/total, edit_dist/total, per/total, binary_metrics(binary_stats)
 
 #################
 from math import ceil
